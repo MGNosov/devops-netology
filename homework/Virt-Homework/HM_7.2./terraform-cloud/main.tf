8478d1aefa3eb0c9bc1a974331bf79fe13f1fe37
Maksim Nosov <mgnosov@Maksims-MacBook-Pro.local>
2022-07-06 17:58:31 +0300

    Home work 7.2.

---
+++ homework/Virt-Homework/HM_7.2./terraform-cloud/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = "IAM_TOKEN"
+  cloud_id  = "b1g9cb14mht57dso4pco"
+  folder_id = "b1gi5ufjf161iudar0ce"
+  zone      = "ru-central1-a"
+}
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 8
+    memory = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.centos-7-base}"
+      name        = "root-node01"
+      type        = "network-nvme"
+      size        = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
