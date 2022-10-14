3f52826dea155373fe160198b4de471ad0e0c24a
MGNosov <95288628+MGNosov@users.noreply.github.com>
2022-10-15 01:11:50 +0300

    Update main.tf

--- Diploma_project/src/Terrafrom/bucket/main.tf
+++ Diploma_project/src/Terrafrom/bucket/main.tf
@@ -7,14 +7,14 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAAnxe1uAATuwbFCSoUwM0v_p5OSKy3TFf0"
+  token     = "AQA************Ff0"
   cloud_id  = "b1g9cb14mht57dso4pco"
   folder_id = "b1gi5ufjf161iudar0ce>"
   zone      = "ru-central1-a"
 }
 
 resource "yandex_storage_bucket" "test" {
-  access_key = "YCAJEfgYIo7o_SSfZ-O-Tfwkj"
-  secret_key = "YCN0n7ocw6ilLq1bVqDEXUTBQxBbNL8u-Ck3SQqI"
+  access_key = "YCA**************wkj"
+  secret_key = "YCN****************QqI"
   bucket = "bin1989"
 }
