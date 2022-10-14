1cc82c0b1b375385761cdc288242381e7ac4350a
MGNosov <95288628+MGNosov@users.noreply.github.com>
2022-10-15 01:12:33 +0300

    Update provider.tf

--- Diploma_project/src/Terrafrom/provider.tf
+++ Diploma_project/src/Terrafrom/provider.tf
@@ -10,8 +10,8 @@ terraform {
    bucket = "bin1989"
    region = "ru-central1"
    key = "~/terraform.tfstate"
-   access_key = "YCAJEfgYIo7o_SSfZ-O-Tfwkj"
-   secret_key = "YCN0n7ocw6ilLq1bVqDEXUTBQxBbNL8u-Ck3SQqI"
+   access_key = "YCA***************wkj"
+   secret_key = "YCN**********************QqI"
   
    skip_region_validation = true
    skip_credentials_validation = true
@@ -20,7 +20,7 @@ terraform {
 
 
 provider "yandex" {
-  token = "AQAAAAAnxe1uAATuwbFCSoUwM0v_p5OSKy3TFf0"
+  token = "AQA*********************Ff0"
   cloud_id  = "b1g9cb14mht57dso4pco"
   folder_id = "b1gi5ufjf161iudar0ce"
   zone = "ru-central1-a"
